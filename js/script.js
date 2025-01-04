ce7ba6e9b35d31a50d0c74755b146686a5e08065
Jozef Kudrna <jojo25011990@gmail.com>
2025-01-04 15:32:16 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const coffee = document.querySelector(".coffee__mug");
+
+window.addEventListener("DOMContentLoaded", () => {
+	setTimeout(() => {
+		coffee.style.opacity = "1";
+	}, 500);
+	// coffee.style.opacity = "1";
+});
